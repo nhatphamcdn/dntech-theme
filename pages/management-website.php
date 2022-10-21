e4e9c08624596f345375bf7c6e9741ce29054f69
nhatpham-myfave <macos@macOSs-MacBook-Pro.local>
2022-10-21 22:36:29 +0700

    add management web page

--- pages/management-website.php
+++ pages/management-website.php
@@ -13,31 +13,122 @@ get_template_part('template-parts/head-banner');
 get_template_part('template-parts/breadcrumb');
 
 ?>
-<div class="container-xxl subpage mt-4 pt-4">
+
+<!-- Section About us start -->
+<div class="container-xxl py-5">
     <div class="container">
-        <div class="px-md-4 pb-5 px-lg-5">
-            <div class="row gx-5">
-                <div class="col-md-4 pe-md-4">
-                    <div class="mx-auto text-left">
-                        <div class="d-inline-block text-primary mb-2">LIÊN HỆ CHÚNG TÔI</div>
-                        <h2 class="mb-4 section-heading-sm">Bạn có câu hỏi? Liên hệ ngay!</h2>
-                    </div>
-                    <div class="pe-md-5">
-                        <?php get_template_part('template-parts/contact-info'); ?>
-                    </div>
+        <div class="px-md-4 px-lg-5">
+            <div class="row gx-5 align-items-center justify-content-center flex-column-reverse flex-md-row">
+                <div class="col-lg-6">
+                    <img class="img-fluid" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/content/web-management/web-manage.png" alt="Web Design">
                 </div>
-                <div class="col-md-7 ps-md-5">
-                    <div class="main-content">
-                        <?php the_content(); ?>
+                <div class="col-lg-6">
+                    <div class="px-0">
+                        <h2 class="mb-4 section-heading">
+                            Quản trị Website tại Đà Nẵng
+                        </h2>
+                        <div>
+                            <p>
+                                DanangTech cung cấp dịch vụ quản trị Website tại Đà Nẵng.
+                            </p>
+                            <p>
+                                Với chi phí tốt nhất, đảm bảo việc chăm sóc Website hay quản trị Website được thực hiện thường xuyên giúp Website hoạt động một cách hiệu quả nhất.
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
-<div class="maps">
-    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/map.png" alt="Liên hệ">
+<!-- Section About us end -->
+
+<!-- Section Service start -->
+<div class="section-with-bg">
+    <div class="container-xxl py-5">
+        <div class="container">
+            <div class="px-md-4 px-lg-5">
+                <div class="mx-auto text-center">
+                    <h2 class="mb-5 section-heading">Lợi ích của việc quản trị Website</h2>
+                </div>
+                <div class="row g-5">
+                    <div class="col-lg-4 col-md-6">
+                        <a href="/thiet-ke-website-tai-da-nang/">
+                            <div class="card-item pointer h-100">
+                                <div class="d-flex justify-content-center">
+                                    <div class="card-icon">
+                                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/webdesign.svg" alt="Thiết kế website" />
+                                    </div>
+                                </div>
+                                <div class="card-content text-center">
+                                    <h5 class="mb-3">Thiết kế website</h5>
+                                    <span>
+                                        Xây dựng một website chuyên nghiệp tốt nhất đại diện cho doanh nghiệp với giá
+                                        phù hợp
+                                    </span>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <a href="/marketing-online-da-nang/">
+                            <div class="card-item pointer h-100">
+                                <div class="d-flex justify-content-center">
+                                    <div class="card-icon">
+                                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/mkt.svg" alt="Marketing Online" />
+                                    </div>
+                                </div>
+                                <div class="card-content text-center">
+                                    <h5 class="mb-3">Marketing Online</h5>
+                                    <span>
+                                        Quảng bá doanh nghiệp hàng tháng trên Internet để tiếp cận khách hàng đang tìm
+                                        kiếm sản phẩm, dịch vụ của bạn
+                                    </span>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <a href="/quan-tri-website-tai-da-nang/">
+                            <div class="card-item pointer h-100">
+                                <div class="d-flex justify-content-center">
+                                    <div class="card-icon">
+                                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/manage.svg" alt="Quản trị Website" />
+                                    </div>
+                                </div>
+                                <div class="card-content text-center">
+                                    <h5 class="mb-3">Quản trị Website</h5>
+                                    <span>
+                                        Đảm bảo việc chăm sóc, quản trị Website được thực hiện thường xuyên giúp Website
+                                        hoạt động một cách hiệu quả nhất.
+                                    </span>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+<!-- Section Service end -->
+
+<!-- Section Service start -->
+<div class="container-xxl py-5">
+    <div class="container">
+        <div class="px-md-4 px-lg-5">
+            <div class="mx-auto text-center">
+                <h2 class="mb-5 section-heading">Dịch vụ quản trị website tại Đà Nẵng </h2>
+            </div>
+            <div class="row g-5">
+
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Section Service end -->
+
+
 <?php
 get_footer(); //displays footer
 
--- pages/marketing-online.php
+++ pages/marketing-online.php
@@ -13,31 +13,143 @@ get_template_part('template-parts/head-banner');
 get_template_part('template-parts/breadcrumb');
 
 ?>
-<div class="container-xxl subpage mt-4 pt-4">
-    <div class="container">
-        <div class="px-md-4 pb-5 px-lg-5">
-            <div class="row gx-5">
-                <div class="col-md-4 pe-md-4">
-                    <div class="mx-auto text-left">
-                        <div class="d-inline-block text-primary mb-2">LIÊN HỆ CHÚNG TÔI</div>
-                        <h2 class="mb-4 section-heading-sm">Bạn có câu hỏi? Liên hệ ngay!</h2>
-                    </div>
-                    <div class="pe-md-5">
-                        <?php get_template_part('template-parts/contact-info'); ?>
+
+<!-- start section-online marketing -->
+<div class="py-5">
+    <div class="mt-2 mt-md-4">
+        <div class="row gx-5 align-items-center justify-content-center flex-column-reverse flex-md-row">
+            <div class="col-lg-6 mb-0">
+                <div class="container-xxl py-md-5">
+                    <div class="container">
+                        <div class="px-md-4">
+                            <div class="align-content content-center content-center-width-large content-center-border-all content-center-shadow">
+                                <h3 class="mb-4 section-heading h3 mt-5 mt-md-0">Online Marketing</h3>
+                                <div class="mb-4">
+                                    <ul class="paragraph-color list">
+                                        <li class="mb-2">Tối ưu các công cụ tìm kiếm như Google, Cốc Cốc, Yahoo, Bing</li>
+                                        <li class="mb-2">Dễ dàng thiết lập hồ sơ địa phương trên Google Business</li>
+                                        <li class="mb-2">Lập chỉ mục trên công cụ tìm kiếm Google</li>
+                                        <li class="mb-2">Công cụ hỗ trợ tối ưu hoá SEO như: cụm từ khóa, thẻ alt, thẻ meta keywords, thẻ meta desciprion, thẻ tiêu đề và nội dung</li>
+                                        <li class="mb-2">Công cụ chia sẻ trên mạng xã hội phục vụ kế hoạch marketing</li>
+                                        <li class="mb-2">Dễ dàng đưa wesbite doanh nghiệp vào Google Maps</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div class="col-md-7 ps-md-5">
-                    <div class="main-content">
-                        <?php the_content(); ?>
+            </div>
+            <div class="col-lg-6 align-content mb-0">
+                <img class="w-100" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/content/marketing-online/service-marketing.png" alt="Service marketing online">
+            </div>
+        </div>
+    </div>
+</div>
+<!-- end section-online marketing -->
+
+<!-- Section About us start -->
+<div class="container-xxl py-5">
+    <div class="container">
+        <div class="px-md-4 px-lg-5">
+            <div class="row gx-5 align-items-center justify-content-center flex-column-reverse flex-md-row">
+                <div class="col-lg-6">
+                    <img class="img-fluid" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/content/marketing-online/concept.png" alt="Web Design">
+                </div>
+                <div class="col-lg-6">
+                    <div class="px-0">
+                        <h2 class="mb-4 section-heading">
+                            Online Marketing là gì?
+                        </h2>
+                        <div>
+                            <p>Online Marketing, hay Internet Marketing (tiếp thị trên Internet) cho phép bạn quảng bá sản phẩm và dịch vụ trực tuyến với chi phí thấp hơn so với các hình thức quảng cáo truyền thống.
+                                <br />
+                                <br />
+                                Online Marketing sẽ giúp bạn phát triển kinh doanh tốt nhất trong thời kỳ phát triển mạnh của Internet hiện nay, giúp mang đến các khách hàng tiềm năng, những người hàng ngày đang tìm kiếm các sản phẩm và dịch vụ mà doanh nghiệp bạn đang kinh doanh.
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
-<div class="maps">
-    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/map.png" alt="Liên hệ">
+<!-- Section About us end -->
+<!-- start section-highlight -->
+<div class="">
+    <div class="container-xxl py-md-5">
+        <div class="container-xxl">
+            <div class="px-md-4 px-lg-5 mt-2 mt-md-4">
+                <div class="row gx-5 align-items-center justify-content-center flex-column-reverse flex-md-row">
+                    <div class="col-lg-6 align-content content-left">
+                        <h3 class="mb-4 section-heading h3 mt-5 mt-md-0">Tại sao bạn cần Online Marketing?</h3>
+                        <div class="mb-4">
+                            <p>
+                                Rất nhiều khách hàng sử dụng Internet cho việc tìm kiếm những thứ họ cần trước khi họ có thể gọi hoặc ghé thăm một cửa hàng. Theo một thống kê từ eMarketer về các tìm kiếm trong lĩnh vực kinh doanh tại địa phương, 86% khách hàng sau khi tìm kiếm thứ mình cần trên Internet sẽ gọi điện hoặc ghé đến cửa hàng, trong đó 61% sẽ mua hàng.
+                                <br />
+                                <br />
+                                Một khách hàng tiềm năng có thể không biết tên doanh nghiệp và địa chỉ Website của bạn, thay vào đó, họ sẽ gõ từ khóa vào công cụ tìm kiếm để tìm sản phẩm mà họ muốn. Sử dụng hiệu quả tiếp thị Internet có thể giúp công ty của bạn hiển thị trên các công cụ tìm kiếm như Google, Cốc Cốc, Bing, Yahoo khi một khách hàng tiềm năng đang tìm kiếm sản phẩm và dịch vụ của bạn.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 align-content">
+                        <img class="img-fluid" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/content/marketing-online/why-need.png" alt="About Us">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+<!-- end section-highlight -->
+<!-- Section why choose start -->
+<div class="container-xxl py-5">
+    <div class="container">
+        <div class="px-md-4 px-lg-5">
+            <div class="row g-5 align-items-end">
+                <div class="col-lg-6">
+                    <img class="img-fluid" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/content/marketing-online/types.png" alt="Target">
+                </div>
+                <div class="col-lg-6">
+                    <div class="px-0 px-md-4">
+                        <h3 class="mb-4 section-heading h3">Các loại hình quảng cáo trực tuyến của Online Marketing?</h3>
+                        <div class="mb-4">
+                            <p>
+                                <strong>Quảng cáo trả tiền theo click (Pay Per Click): </strong>
+                            </p>
+                            <p>
+                                Quảng cáo xuất hiện nhanh chóng để tiếp cận khách hàng tại địa phương, khu vực nhất định hoặc quốc gia. Với doanh nghiệp địa phương, hình thức quảng cáo này sẽ mang đến khách hàng tiềm năng trong khu vực địa phương, ngay khi các khách hàng đó thực hiện tìm kiếm để mua hàng.
+                            </p>
+                            <p>
+                                <strong>SEO (Search Engine Optimization)</strong>
+                            </p>
+                            <p>
+                                Google là công cụ tìm kiếm được sử dụng nhiều nhất, được tin tưởng liên kết kết quả tìm kiếm đến các doanh nghiệp có uy tín để giúp các khách hàng tìm được những nơi tốt nhất. Để có được vị trí trong top kết quả kiếm của Google phải mất nhiều tháng để tối ưu, xây dựng nội dung, thu hút người vào Website… Công việc này được gọi là SEO. Nhìn chung, chúng ta sẽ tạo uy tín với công cụ tìm kiếm và giúp bạn chuyển lên các vị trí xếp hạng cao hơn trong kết quả tìm kiếm. Nếu bạn đã có một Website và muốn cải thiện kết quả của nó thì SEO là giải pháp không thể thiếu.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Section Why choose end -->
+
+<!-- Section optimize start -->
+<div class="container-xxl py-5">
+    <div class="container">
+        <div class="px-md-4 px-lg-5">
+            <div class="mx-auto mw-md-60 text-center">
+                <h2 class="mb-5 mb-md-3 section-heading">Sử dụng Online Marketing như thế nào là tốt nhất trong kinh doanh của doanh nghiệp?</h2>
+
+                <p>Để thành công trong chiến dịch Internet Marketing, DanangTech cung cấp cho khách hàng một số giải pháp phù hợp cho từng giai đoạn và từng nhu cầu của khách hàng.</p>
+            </div>
+            <div class="content">
+                <?php the_content(); ?>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Section Why choose end -->
+
 <?php
 get_footer(); //displays footer
 
