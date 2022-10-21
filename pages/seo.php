3ff5096357faa68a338a3d2645499a90f5fd3431
nhatpham-myfave <macos@macOSs-MacBook-Pro.local>
2022-10-21 23:55:13 +0700

    add seo page

--- pages/seo.php
+++ pages/seo.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Template Name: Da Nang Tech Hosting
+ * Template Name: Da Nang Tech SEO
  *
  * @package Genesis\Templates
  */
@@ -13,31 +13,156 @@ get_template_part('template-parts/head-banner');
 get_template_part('template-parts/breadcrumb');
 
 ?>
-<div class="container-xxl subpage mt-4 pt-4">
+
+<!-- start optimize -->
+
+<div class="container-xxl py-5">
     <div class="container">
-        <div class="px-md-4 pb-5 px-lg-5">
-            <div class="row gx-5">
-                <div class="col-md-4 pe-md-4">
-                    <div class="mx-auto text-left">
-                        <div class="d-inline-block text-primary mb-2">LIÊN HỆ CHÚNG TÔI</div>
-                        <h2 class="mb-4 section-heading-sm">Bạn có câu hỏi? Liên hệ ngay!</h2>
-                    </div>
-                    <div class="pe-md-5">
-                        <?php get_template_part('template-parts/contact-info'); ?>
+        <div class="px-md-4 px-lg-5">
+            <div class="row g-5 align-items-center justify-content-center flex-column-reverse flex-md-row">
+                <div class="col-lg-6">
+                    <div class="px-0">
+                        <h2 class="mb-4 section-heading">
+                            Tại sao cần tối ưu SEO với Google?
+                        </h2>
+                        <div>
+                            <p>Để doanh nghiệp của bạn hiển thị ở phía trên các kết quả của công cụ tìm kiếm Google thì cần phải thực hiện quy trình tối ưu SEO, hiện nay nhiều doanh nghiệp đã quan tâm nhiều hơn đến việc thiết kế trang web và tối ưu SEO để doanh nghiệp có thể được tìm thấy trên các bộ máy tìm kiếm bởi người dùng.</p>
+                            <p>Mỗi ngày đều có rất nhiều người sử dụng công cụ Google để tìm kiếm các sản phẩm, dịch vụ mình quan tâm và sau đó là mua hàng. Quá trình tối ưu SEo cho website được gọi là “Search Engine Optimization” (SEO) và chắc chắn sẽ mang lại hiệu quả thực sự cho doanh nghiệp bạn.</p>
+                            <p>Khách với quảng cáo Google Adwords, giải pháp tối ưu hóa SEO với bộ máy tìm kiếm Google cần phải thực hiện theo từng bước, đúng quy trình, thời gian thực hiện sẽ lâu hơn, tuy nhiên kết quả thứ hạng mang lại sẽ lâu dài hơn.</p>
+                        </div>
                     </div>
                 </div>
-                <div class="col-md-7 ps-md-5">
-                    <div class="main-content">
-                        <?php the_content(); ?>
+                <div class="col-lg-6">
+                    <img class="img-fluid" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/content/seo/optimize.png" alt="Tại sao cần tối ưu SEO với Google?">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- end section-optimize -->
+
+<!-- Section Loi ich -->
+<div class="">
+    <div class="py-md-5">
+        <div class="px-md-4 px-lg-5 mt-2 mt-md-4">
+            <div class="row gx-5 align-items-center justify-content-center flex-column-reverse flex-md-row">
+                <div class="col-lg-6 align-content">
+                    <img class="w-100" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/content/seo/idea.png" alt="Lợi ích SEO mang đến cho bạn">
+                </div>
+                <div class="col-lg-6 align-content content-right">
+                    <h3 class="mb-4 section-heading h3 mt-5 mt-md-0">Lợi ích SEO mang đến cho bạn</h3>
+                    <div class="mb-4">
+                        <div class="check-list-square">
+                            <ul class="list">
+                                <li class="mb-2">Tăng sự hiện diện của website trên Google: Tăng thứ hạng cho các từ khóa quan trọng để tăng lưu lượng truy cập vào website</li>
+                                <li class="mb-2">Kế hoạch thực hiện tối ưu SEO website: Giúp khách hàng thực hiện từng bước đúng cho Website của mình</li>
+                                <li class="mb-2">Tối ưu tìm kiếm địa phượng: Có được xếp hạng tại địa phương (Đà Nẵng hoặc các khu vực khác) khi tìm kiếm trên Google</li>
+                                <li>Chi phí thấp: So với các giải pháp Online Marketing khác, SEO là một giải pháp Online Marketing chi phí thấp nhưng mang lại hiệu quả lâu dài</li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
-<div class="maps">
-    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/map.png" alt="Liên hệ">
+<!-- Section loi ich -->
+
+<!-- Section gioi thieu -->
+<div class="container-xxl">
+    <div class="container">
+        <div class="py-md-5">
+            <div class="px-md-4 px-lg-5 mt-2 mt-md-4">
+                <div class="row gx-5 align-items-center justify-content-center flex-column-reverse flex-md-row">
+                    <div class="col-lg-6 align-content">
+                        <h3 class="mb-4 section-heading h3 mt-5 mt-md-0">Giới thiệu dịch vụ SEO tại Đà Nẵng Tech</h3>
+                        <div class="mb-4">
+                            <div class="check-list-square">
+                                <ul class="list">
+                                    <li class="mb-2">Phân tích và lựa chọn từ khóa tối ưu nhất trong lĩnh vực của doanh nghiệp</li>
+                                    <li class="mb-2">Tối ưu hóa mã nguồn của website chuẩn SEO để Google có thể đọc website dễ dàng</li>
+                                    <li class="mb-2">Xây dựng liên kết chất lượng và có liên quan đến lĩnh vực hoạt động của doanh nghiệp để công cụ tìm kiếm đánh giá cao chất lượng website</li>
+                                    <li>Tư vấn, đánh giá kết quả và cải thiện SEO theo hướng tốt nhất cho doanh nghiệp</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 align-content">
+                        <img class="w-100" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/content/seo/intro.png" alt="Giới thiệu dịch vụ SEO tại Đà Nẵng Tech">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+<!-- Section gioi thieu end -->
+
+<!-- Start pricing table -->
+<div class="container-xxl pb-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="plan mb-3">
+                    <h2 class="plan-heading">Free</h2>
+                    <h3 class="plan-price">
+                        400.000
+                        <span>VND/ tháng</span>
+                    </h3>
+                    <span class="divider"></span>
+                    <div class="text-start">
+                        <ul class="ticks-list">
+                            <li class="mb-3">Kế hoạch tối ưu step-by-step</li>
+                            <li class="mb-3">Tư vấn từ khóa SEO hiệu quả</li>
+                            <li class="mb-3">Đưa từ khóa lên top Google tại Đà Nẵng</li>
+                            <li class="mb-3">Đưa từ khóa lên top Google tại Đà Nẵng</li>
+                        </ul>
+                    </div>
+                    <a href="#" class="mt-3 btn btn-outline-primary">Đăng Ký</a>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="plan active mb-3">
+                    <h2 class="plan-heading">Free</h2>
+                    <h3 class="plan-price">
+                        400.000
+                        <span>VND/ tháng</span>
+                    </h3>
+                    <span class="divider"></span>
+                    <div class="text-start">
+                        <ul class="ticks-list">
+                            <li class="mb-3">Kế hoạch tối ưu step-by-step</li>
+                            <li class="mb-3">Tư vấn từ khóa SEO hiệu quả</li>
+                            <li class="mb-3">Đưa từ khóa lên top Google tại Đà Nẵng</li>
+                            <li class="mb-3">Đưa từ khóa lên top Google tại Đà Nẵng</li>
+                        </ul>
+                    </div>
+                    <a href="#" class="mt-3 btn btn-primary">Đăng Ký</a>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="plan mb-3">
+                    <h2 class="plan-heading">Free</h2>
+                    <h3 class="plan-price">
+                        400.000
+                        <span>VND/ tháng</span>
+                    </h3>
+                    <span class="divider"></span>
+                    <div class="text-start">
+                        <ul class="ticks-list">
+                            <li class="mb-3">Kế hoạch tối ưu step-by-step</li>
+                            <li class="mb-3">Tư vấn từ khóa SEO hiệu quả</li>
+                            <li class="mb-3">Đưa từ khóa lên top Google tại Đà Nẵng</li>
+                            <li class="mb-3">Đưa từ khóa lên top Google tại Đà Nẵng</li>
+                        </ul>
+                    </div>
+                    <a href="#" class="mt-3 btn btn-outline-primary">Đăng Ký</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- end pricing table -->
+
 <?php
 get_footer(); //displays footer
 
